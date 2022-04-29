838fbc0f74815da1c520a5366f9bbf88382c4824
BHT256 <30569589+BHT256@users.noreply.github.com>
2022-04-29 21:34:42 +0800

    模糊查找(初型)
    利用memchr函数以及memcmp以及<string>头文件中的相关函数实现简单的模糊查找。可用于需要检索大量信息的系统中。由于是初型，并未考虑时间复杂度相关问题。

---
+++ 模糊查找(初型).cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    char str[] = "programming charm\0"; //源字符串
+    printf("输入要搜索的某个字符:\n");
+    fflush(stdin); //重刷新缓存区 确保在下一次输入时不会有额外的字符进入标准输入流中
+    char str2[20];              //要查找的字符串
+    fgets(str2, 19, stdin);
+    char* res;
+    res = (char*)memchr(str, str2[0], strlen(str));  //根据要查找的字符串第一个字符，切割源字符串
+    if (res == NULL)
+    {
+        printf("找不到\n");
+        return 0;
+    }
+    int n;
+    while (1)
+    {
+        n = memcmp(res, str2, strlen(str2) - 1); //比较
+        if (n != 0)
+        {
+            if (strlen(res) <= strlen(str2))  //切割出的字符串小于要查找字符串的长度
+            {
+                printf("找不到\n");
+                return 0;
+            }
+            else
+            {
+                //根据要查找的第一个字符继续切割
+                res = (char*)memchr(res + 1, str2[0], strlen(res));
+                if (res == NULL)
+                {
+                    printf("找不到\n");
+                    return 0;
+                }
+            }
+        }
+        else
+        { //如果n = 0，找到
+            printf("已找到%s\n", str2);
+            return 0;
+        }
+    }
+}
